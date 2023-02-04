695c95a6709c297367ca216b8d863bbe1ea24fed
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-05 00:07:47 +0500

    Add files via upload

---
+++ task 7.js
@@ -0,0 +1,7 @@
+var a =  "Variable names can only contain numbers, $ and _. For example $my _1stVariable."+ "<br>";
+var b = "Variables must begin with a letter, $ or _. For example : $name, _name or name" + "<br>";
+var c  = "Variable names are case sensitive" + "<br>";
+var d = "Variable names should not be JS keywords"
+
+
+document.write(a, b, c, d,);
