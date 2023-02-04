571b9b3ebfdb7a6c4b9b333684b8daaa1c60d24f
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-05 01:02:40 +0500

    Add files via upload

---
+++ task 9.js
@@ -0,0 +1,7 @@
+var a = 8;
+var b = 5;
+
+document.write("8 - 5 = " + (a - b) + "<br>");
+document.write("8 * 5 = " + a*b + "<br>");
+document.write("8 / 5 = " + a/b + "<br>");
+document.write("8 % 5 = " + a%b + "<br>");
